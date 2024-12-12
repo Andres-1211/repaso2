4209926ad81bfe7c28bd745e5685399a96a1875b
Andres-1211 <189870141+Andres-1211@users.noreply.github.com>
2024-12-12 12:56:54 -0500

    ejercicio 1

---
+++ main.py
@@ -0,0 +1,7 @@
+#Escribir una función que reciba un número entero positivo y devuelva su factorial.
+
+n = int(input("ingrese un numero "))
+
+import math
+
+print(f"el factorial es",math.factorial(n))
